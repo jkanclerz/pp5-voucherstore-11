6349eeafbbd5f2e3959382919830083e970f6cc7
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-11-10 13:25:47 +0100

    introduce jdpc product storage

--- src/test/java/pl/jkanclerz/voucherstore/productcatalog/JDBCProductStorageTest.java
+++ src/test/java/pl/jkanclerz/voucherstore/productcatalog/JDBCProductStorageTest.java
@@ -1,5 +1,6 @@
 package pl.jkanclerz.voucherstore.productcatalog;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.math.BigDecimal;
-import java.util.UUID;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 
@@ -19,9 +20,21 @@ public class JDBCProductStorageTest {
     @Autowired
     JdbcTemplate jdbcTemplate;
 
+    @Before
+    public void setUp() {
+        jdbcTemplate.execute("DROP TABLE products_catalog__products IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE `products_catalog__products` (" +
+                "`id` varchar(100) NOT NULL," +
+                "`description` varchar(255)," +
+                "`picture` varchar(150)," +
+                "`price` DECIMAL(12,2)," +
+                "PRIMARY KEY (id)" +
+                ");");
+    }
+
     @Test
-    public void itAllowStorePproduct() {
-        var product = new Product(UUID.randomUUID());
+    public void itAllowStoreProduct() {
+        var product = ProductFixtures.draftProduct();
         ProductStorage jdbcStorage = thereIsJDBCProductStorage();
 
         jdbcStorage.save(product);
@@ -33,8 +46,30 @@ public class JDBCProductStorageTest {
     }
 
     @Test
-    public void itAllowLoadAllProducts() {
+    public void thereIsEmptyWheNoProduct() {
+        ProductStorage jdbcStorage = thereIsJDBCProductStorage();
+        assertThat(jdbcStorage.getById("not_exists"))
+                .isEmpty();
+    }
 
+    @Test
+    public void itAllowLoadAllProducts() {
+        var p1 = ProductFixtures.readyToSellProduct("p1", 20.20);
+        var p2 = ProductFixtures.readyToSellProduct("p2", 20.20);
+        var p3 = ProductFixtures.draftProduct();
+
+        ProductStorage jdbcStorage = thereIsJDBCProductStorage();
+
+        jdbcStorage.save(p1);
+        jdbcStorage.save(p2);
+
+        List<Product> products = jdbcStorage.allPublishedProducts();
+
+        assertThat(products)
+                .hasSize(2)
+                .extracting(Product::getId)
+                .contains(p1.getId())
+                .doesNotContain(p3.getId());
     }
 
     private ProductStorage thereIsJDBCProductStorage() {
