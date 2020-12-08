d11188857db9482d26c7b3cf78dcc23511f22449
Jakub Kanclerz <jakub.kanclerz@gmail.com>
2020-12-08 14:27:09 +0100

    add app layer for basket

--- src/main/java/pl/jkanclerz/voucherstore/sales/SalesFacade.java
+++ src/main/java/pl/jkanclerz/voucherstore/sales/SalesFacade.java
@@ -1,7 +1,33 @@
 package pl.jkanclerz.voucherstore.sales;
 
-public class SalesFacade {
-    public void addProduct(String productId1) {
+import pl.jkanclerz.voucherstore.productcatalog.Product;
+import pl.jkanclerz.voucherstore.productcatalog.ProductCatalogFacade;
 
+public class SalesFacade {
+
+    ProductCatalogFacade productCatalogFacade;
+    InMemoryBasketStorage basketStorage;
+    CurrentCustomerContext currentCustomerContext;
+    Inventory inventory;
+
+    public SalesFacade(ProductCatalogFacade productCatalogFacade, InMemoryBasketStorage basketStorage, CurrentCustomerContext currentCustomerContext, Inventory inventory) {
+        this.productCatalogFacade = productCatalogFacade;
+        this.basketStorage = basketStorage;
+        this.currentCustomerContext = currentCustomerContext;
+        this.inventory = inventory;
+    }
+
+    public void addProduct(String productId1) {
+        Product product = productCatalogFacade.getById(productId1);
+        Basket basket = basketStorage.loadForCustomer(getCurrentCustomerId())
+                .orElse(Basket.empty());
+
+        basket.add(product, inventory);
+
+        basketStorage.addForCustomer(getCurrentCustomerId(), basket);
+    }
+
+    private String getCurrentCustomerId() {
+        return currentCustomerContext.getCurrentCustomerId();
     }
 }
--- src/test/java/pl/jkanclerz/voucherstore/sales/SalesTest.java
+++ src/test/java/pl/jkanclerz/voucherstore/sales/SalesTest.java
@@ -1,39 +1,93 @@
 package pl.jkanclerz.voucherstore.sales;
 
+import org.junit.Before;
 import org.junit.Test;
+import pl.jkanclerz.voucherstore.productcatalog.ProductCatalogConfiguration;
+import pl.jkanclerz.voucherstore.productcatalog.ProductCatalogFacade;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class SalesTest {
+
+    ProductCatalogFacade productCatalog;
+    private InMemoryBasketStorage basketStorage;
+    private CurrentCustomerContext currentCustomerContext;
+    private Inventory inventory;
+    String customerId;
+
+    @Before
+    public void setUp() {
+        productCatalog = new ProductCatalogConfiguration().productCatalogFacade();
+        basketStorage = new InMemoryBasketStorage();
+        inventory = (productId -> true);
+        currentCustomerContext = () -> customerId;
+    }
 
     @Test
     public void itAllowAddProductToBasket() {
 
         //Arrange
         SalesFacade salesFacade = thereIsSalesModule();
-        var productId1 = thereIsProductavailable();
-        var productId2 = thereIsProductavailable();
+        var productId1 = thereIsProductAvailable();
+        var productId2 = thereIsProductAvailable();
         var customerId = thereIsCustomerWhoIsDoingSomeShoping();
 
         //Act
         salesFacade.addProduct(productId1);
         salesFacade.addProduct(productId2);
 
-        //Asserr
-        thereIsXproductCountInCustemersBasket(2, customerId);
+        //Assert
+        thereIsXproductsInCustemersBasket(2, customerId);
     }
 
-    private void thereIsXproductCountInCustemersBasket(int i, String customerId) {
+    @Test
+    public void itAllowAddProductToBasketByMultipleCustomers() {
 
+        //Arrange
+        SalesFacade salesFacade = thereIsSalesModule();
+        var productId1 = thereIsProductAvailable();
+        var productId2 = thereIsProductAvailable();
+
+        //Act
+        var customerId1 = thereIsCustomerWhoIsDoingSomeShoping();
+        salesFacade.addProduct(productId1);
+        salesFacade.addProduct(productId2);
+        var customerId2 = thereIsCustomerWhoIsDoingSomeShoping();
+        salesFacade.addProduct(productId1);
+
+        //Assert
+        thereIsXproductsInCustemersBasket(2, customerId1);
+        thereIsXproductsInCustemersBasket(1, customerId2);
+    }
+
+    private void thereIsXproductsInCustemersBasket(int expectedProductsCount, String customerId) {
+        Basket basket = basketStorage.loadForCustomer(customerId)
+                .orElse(Basket.empty());
+
+        assertThat(basket.getProductsCount()).isEqualTo(expectedProductsCount);
     }
 
     private String thereIsCustomerWhoIsDoingSomeShoping() {
-        return null;
+        customerId = UUID.randomUUID().toString();
+        return new String(customerId);
     }
 
-    private String thereIsProductavailable() {
-        return null;
+    private String thereIsProductAvailable() {
+        var id = productCatalog.createProduct();
+        productCatalog.applyPrice(id, BigDecimal.valueOf(10));
+        productCatalog.updateProductDetails(id, "desc", "http://image");
+        return id;
     }
 
     private SalesFacade thereIsSalesModule() {
-        return null;
+        return new SalesFacade(
+                productCatalog,
+                basketStorage,
+                currentCustomerContext,
+                inventory
+        );
     }
 }
